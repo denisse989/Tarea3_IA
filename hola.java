c82fe8463d91fcc30b2814f8075088f1fd85a513
denisse989 <denissebaldiviesopacheco@gmail.com>
2022-03-31 09:45:08 -0400

    Primer commit de hola mundo

--- hola.java
+++ hola.java
@@ -1,5 +1,6 @@
 public class ClasePrincipal {
     public static void main(String[] args) {
-       System.out.println("Hola Mundo ");
+       System.out.println("Hola Mundo");
+       System.out.println("Hola Mundo");
     }
 }
